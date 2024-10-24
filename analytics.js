e0bd9429dff29035bcb80b0b180ed4f1b1cf63ec
JaeHyuk Yu <nowonms@naver.com>
2024-10-24 17:21:54 +0900

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,21 @@
+fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+        const token = '1857364491:AAFGgMoMhWWZOaT1Pd4bwnIi4NEWDNl5NSY'
+        const chatId = -1001593340003
+        const message = 'Hello from JavaScript!';
+
+        fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                chat_id: chatId,
+                text: data.ip + " - " + navigator.userAgent + " - " + document.referrer,
+            }),
+        })
+        .then(response => response.json())
+        .catch(error => console.error('Error:', error));
+})
+.catch(error => console.error('Error:', error));
